6f1fd6ae1bf7b378acf0e3dbafffc970ac02b92e
Robert <robertzwinchell@gmail.com>
2015-05-20 21:24:59 -0600

    was access
    ws access

---
+++ subway26/subway/sub6.js
@@ -0,0 +1,223 @@
+var input_raw = {
+    "LexDiagramId": 3,
+    "LCID": 1033,
+    "DiagramName": "Exchange 2013/Online Administrator",
+    "DiagramDescription": "Exchange Certification",
+    "CreatedBy": "rob.collins@crank211.com",
+    "CreatedAt": "2015-05-04T00:09:52.393",
+    "ModifiedBy": "rob.collins@crank211.com",
+    "ModifiedAt": "2015-05-04T00:09:52.393",
+    "DiagramNodes": [
+        {
+            "DiagramNodeId": 33,
+            "ParentDiagramNodeId": null,
+            "DiagramNodeType": "StartNode",
+            "NodeTitle": "Implement and manage transport",
+            "NodeText": "",
+            "DurationInHours": 32,
+            "ChildNodeGroupId": 1,
+            "ParentNodeGroupId": null,
+            "NodeTickValue": null,
+            "NodeHexColor": "#000000",
+            "DiagramNodeDetailPageId": null,
+            "DiagramConnectorDtos": [
+                {
+                    "DiagramConnectorId": 33,
+                    "StartDiagramNodeId": 33,
+                    "EndDiagramNodeId": 37,
+                    "ConnectorHexColor": "#FF8000",
+                    "DiagramConnectorType": "Basic"
+                }
+            ],
+            "ChildGroup": {
+                "NodeGroupId": 1,
+                "NodeGroupTitle": "Implement and manage transport",
+                "NodeGroupTickValue": null,
+                "ChildDiagramNodeDtos": [
+                    {
+                        "DiagramNodeId": 34,
+                        "ParentDiagramNodeId": null,
+                        "DiagramNodeType": "GroupStartNode",
+                        "NodeTitle": "Design a transport solution",
+                        "NodeText": "",
+                        "DurationInHours": 12,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 1,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [
+                            {
+                                "DiagramConnectorId": 31,
+                                "StartDiagramNodeId": 34,
+                                "EndDiagramNodeId": 35,
+                                "ConnectorHexColor": "#FF8000",
+                                "DiagramConnectorType": "Basic"
+                            }
+                        ],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 35,
+                        "ParentDiagramNodeId": 34,
+                        "DiagramNodeType": "Basic",
+                        "NodeTitle": "Configure and manage transport",
+                        "NodeText": "",
+                        "DurationInHours": 12,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 1,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [
+                            {
+                                "DiagramConnectorId": 32,
+                                "StartDiagramNodeId": 35,
+                                "EndDiagramNodeId": 36,
+                                "ConnectorHexColor": "#FF8000",
+                                "DiagramConnectorType": "Basic"
+                            }
+                        ],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 36,
+                        "ParentDiagramNodeId": 35,
+                        "DiagramNodeType": "GroupEndNode",
+                        "NodeTitle": "Configure and manage hygiene",
+                        "NodeText": "",
+                        "DurationInHours": null,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 1,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [],
+                        "ChildGroup": null
+                    }
+                ]
+            }
+        },
+        {
+            "DiagramNodeId": 37,
+            "ParentDiagramNodeId": 33,
+            "DiagramNodeType": "Basic",
+            "NodeTitle": "Implement and manage transport",
+            "NodeText": "",
+            "DurationInHours": 32,
+            "ChildNodeGroupId": 2,
+            "ParentNodeGroupId": null,
+            "NodeTickValue": null,
+            "NodeHexColor": "#000000",
+            "DiagramNodeDetailPageId": null,
+            "DiagramConnectorDtos": [],
+            "ChildGroup": {
+                "NodeGroupId": 2,
+                "NodeGroupTitle": "Implement and manage mailbox role",
+                "NodeGroupTickValue": null,
+                "ChildDiagramNodeDtos": [
+                    {
+                        "DiagramNodeId": 38,
+                        "ParentDiagramNodeId": null,
+                        "DiagramNodeType": "GroupStartNode",
+                        "NodeTitle": "Plan the Exchange Server 2013 mailbox role",
+                        "NodeText": "",
+                        "DurationInHours": 8,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 2,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [
+                            {
+                                "DiagramConnectorId": 34,
+                                "StartDiagramNodeId": 38,
+                                "EndDiagramNodeId": 39,
+                                "ConnectorHexColor": "#FF8000",
+                                "DiagramConnectorType": "Basic"
+                            }
+                        ],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 39,
+                        "ParentDiagramNodeId": 38,
+                        "DiagramNodeType": "Basic",
+                        "NodeTitle": "Configure and manage the mailbox role",
+                        "NodeText": "",
+                        "DurationInHours": 8,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 2,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [
+                            {
+                                "DiagramConnectorId": 35,
+                                "StartDiagramNodeId": 39,
+                                "EndDiagramNodeId": 40,
+                                "ConnectorHexColor": "#FF8000",
+                                "DiagramConnectorType": "Basic"
+                            }
+                        ],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 40,
+                        "ParentDiagramNodeId": 39,
+                        "DiagramNodeType": "Basic",
+                        "NodeTitle": "Deploy and manage high availablity solutions for the mailbox role",
+                        "NodeText": "",
+                        "DurationInHours": 8,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 2,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 41,
+                        "ParentDiagramNodeId": 40,
+                        "DiagramNodeType": "Basic",
+                        "NodeTitle": "Develop backup and recovery solutions for the mailbox role and public folders",
+                        "NodeText": "",
+                        "DurationInHours": 4,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 2,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [
+                            {
+                                "DiagramConnectorId": 36,
+                                "StartDiagramNodeId": 41,
+                                "EndDiagramNodeId": 42,
+                                "ConnectorHexColor": "#FF8000",
+                                "DiagramConnectorType": "Basic"
+                            }
+                        ],
+                        "ChildGroup": null
+                    },
+                    {
+                        "DiagramNodeId": 42,
+                        "ParentDiagramNodeId": 41,
+                        "DiagramNodeType": "GroupEndNode",
+                        "NodeTitle": "Design, deploy and manage a site resilient Exchange solution",
+                        "NodeText": "",
+                        "DurationInHours": 4,
+                        "ChildNodeGroupId": null,
+                        "ParentNodeGroupId": 2,
+                        "NodeTickValue": null,
+                        "NodeHexColor": "#000000",
+                        "DiagramNodeDetailPageId": null,
+                        "DiagramConnectorDtos": [],
+                        "ChildGroup": null
+                    }
+                ]
+            }
+        }
+    ]
+}
+
